f8b006b414e86acf1e8e93f7068899e22932ca14
Jeswin Kumar <jeswinpk@agilehead.com>
2018-07-01 18:34:10 +0530

    Fix test input

--- src/test/test.ts
+++ src/test/test.ts
@@ -4,9 +4,9 @@ import Response from "../Response";
 
 describe("scuttlespace-cli-common", () => {
   it("creates a response", () => {
-    const resp = new Response("msg", "sender");
+    const resp = new Response("msg", "replyto");
     resp.message.should.equal("msg");
-    resp.replyToId.should.equal("sender");
+    resp.replyToId.should.equal("replyto");
     (typeof resp.timestamp).should.equal("number");
   });
 });
