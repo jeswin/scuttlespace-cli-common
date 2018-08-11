01216b4bda871478810ef5978672ad1745b98309
Jeswin Kumar <jeswinpk@agilehead.com>
2018-08-11 11:15:44 +0530

    Merge files since the code is simple enough

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,29 @@
+import { ICallContext } from "standard-api";
+import Response from "./Response";
+
 export { default as Response } from "./Response";
-export { IMessage, IMessageSource } from "./types";
+
+export interface IMessage {
+  id: string;
+  sender: string;
+  root: string;
+  text: string;
+  timestamp: number;
+}
+
+export interface IMessageSource {
+  get(id: string): Promise<IMessage>;
+}
+
+export interface IConfig {
+  graphqlHost: string;
+  graphqlPort: number;
+}
+
+export type HandlerFunc = (
+  command: string,
+  messageId: string,
+  sender: string,
+  config: IConfig,
+  context: ICallContext
+) => Promise<Response | undefined>;
