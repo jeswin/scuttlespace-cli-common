b1559ec0b283f5307fa665e5d6cd6853d01069cb
Jeswin Kumar <jeswinpk@agilehead.com>
2018-08-13 19:15:33 +0530

    decouple from ssb

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,48 @@
-import { Msg } from "ssb-typescript";
 import { ICallContext } from "standard-api";
 import Response from "./Response";
 
 export { default as Response } from "./Response";
-
-export interface IMessageSource {
-  get(id: string): Promise<Msg<any>>;
-}
-
-export interface IHandlerResponse {
-  message?: string;
-}
 
 export interface IConfig {
   graphqlHost: string;
   graphqlPort: number;
 }
 
+export interface IMessage<TContent> {
+  author: string;
+  branch?: string | string[];
+  channel?: string;
+  content: TContent;
+  key: string;
+  mentions?: string[];
+  root?: string;
+  timestamp: number;
+  type: string;
+}
+
+export interface IMessageSource {
+  get(id: string): Promise<IMessage<any>>;
+}
+
+export interface IHandlerResponse {
+  message?: string;
+}
+
 export interface IScuttleSpaceModule {
   handle(
-    message: Msg<any>,
+    message: IMessage<any>,
     msgSource: IMessageSource
   ): Promise<IHandlerResponse | void>;
 }
 
 export type HandlerFunc = (
-  command: string,
-  messageId: string,
-  sender: string,
+  message: IMessage<any>,
   config: IConfig,
   context: ICallContext
 ) => Promise<Response | undefined>;
 
-export function extractText(source: Msg<any>, botPublicKey: string) {
-  const text =
-    source.value && source.value.content && source.value.content.text;
+export function extractText(source: IMessage<any>, botPublicKey: string) {
+  const text = source && source.content && source.content.text;
 
   return text
     ? text
