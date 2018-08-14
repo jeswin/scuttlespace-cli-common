309a04533c43df368df95ace7412010742b61b01
Jeswin Kumar <jeswinpk@agilehead.com>
2018-08-14 19:42:14 +0530

    Add test for extractText

--- src/test/test.ts
+++ src/test/test.ts
@@ -1,5 +1,6 @@
 import "mocha";
 import "should";
+import { extractText, IMessage } from "..";
 import Response from "../Response";
 
 describe("scuttlespace-cli-common", () => {
@@ -8,5 +9,19 @@ describe("scuttlespace-cli-common", () => {
     resp.message.should.equal("msg");
     resp.replyToId.should.equal("replyto");
     (typeof resp.timestamp).should.equal("number");
+  });
+
+  it("extracts text from a text message", () => {
+    const msg: IMessage<{ text: string }> = {
+      author: "author-id",
+      content: {
+        text: "some-bot-key hello world"
+      },
+      key: "some-msg-key",
+      timestamp: 100,
+      type: "post"
+    };
+    const text = extractText(msg, "some-bot-key");
+    text.should.equal("hello world");
   });
 });
